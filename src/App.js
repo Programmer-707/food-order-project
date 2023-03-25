74db144ceacd4a38fd5c94dbee05f83da496d2eb
Muslimbek Abduvaliev <muslim.0621@mail.ru>
2023-03-25 14:04:54 +0100

    implement cart popup using portal, useState, and some conditions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false)
+
+  const showCart = () => {
+    setIsCartShown(true)
+  }
+
+  const hideCart = () => {
+    setIsCartShown(false)
+  }
+
   return (
     <>
-      <Header />
+      {isCartShown && <Cart onClose={hideCart}/>}
+      <Header onShowCart={showCart}/>
       <main>
         <Meals />
       </main>
