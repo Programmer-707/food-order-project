272ac1791a54d7d52444fbf2ddf978905b471b0d
Muslimbek Abduvaliev <muslim.0621@mail.ru>
2023-03-26 10:17:38 +0200

    implement cart context using useContext, useRef, useReducer

--- src/components/Meals/components/MealItemForm.js
+++ src/components/Meals/components/MealItemForm.js
@@ -1,7 +1,27 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [isAmountValid, setIsAmountValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber)
+  };
+
   const inputParams = {
     id: `amount_${props.id}`,
     type: "number",
@@ -12,9 +32,10 @@ const MealItemForm = (props) => {
   };
 
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={inputParams} />
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input ref={amountInputRef} label="Amount" input={inputParams} />
       <button>+ Add</button>
+      {!isAmountValid && <p>Please enter valid amount</p>}
     </form>
   );
 };
